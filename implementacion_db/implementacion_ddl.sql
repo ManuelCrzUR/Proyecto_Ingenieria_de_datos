f4cabaa96380a37643689460a3391c3305eac417
Manuel Cruz Garrote <manuels.cruz@urosario.edu.co>
2024-05-09 14:46:14 -0500

    Implementación de las tablas clasificacion_superior y informacion_taxonomica de forma correcta y funcional

--- implementacion_db/implementacion_ddl.sql
+++ implementacion_db/implementacion_ddl.sql
@@ -1,13 +1,21 @@
 CREATE TABLE clasificacion_superior (
-	codigo_tax varchar(20),
+	codigo integer,
 	reino varchar(40),
 	filio varchar(40),
 	clase varchar(40),
 	orden varchar(40),
 	familia varchar(40),
-	idenficacion_tax varchar(60),
-	PRIMARY KEY (codigo_tax),
-	FOREIGN KEY (identificacion_tax) REFERENCES clasificar
+	PRIMARY KEY (codigo)
+);
+
+CREATE TABLE informacion_taxonomica (
+	id_taxonomia varchar(40),
+	estado_taxonomico varchar(40),
+	observacion_taxonomica varchar(80),
+	rango_taxonomico varchar(40),
+	codigo_clasificacion integer,
+	PRIMARY KEY (id_taxonomia), 
+	FOREIGN KEY (codigo_clasificacion) REFERENCES clasificacion_superior
 );
 
 CREATE TABLE intraespecificidad_epiteto (
@@ -18,28 +26,4 @@ CREATE TABLE intraespecificidad_epiteto (
 	nombre_vernaculo varchar(60),
 	PRIMARY KEY (codigo_int),
 	FOREIGN KEY (codigo_tax) REFERENCES clasificacion_superior
-);
-
-CREATE TABLE informacion_taxonomica (
-	id_taxonomia varchar(40),
-	estado_tax varchar(40),
-	observacion_tax varchar(80),
-	rango_tax varchar(40),
-	PRIMARY KEY (id_taxonomia)
-);
-
-CREATE TABLE clasificar (
-	identificacion_tax varchar(60),
-	id_informacion_tax varchar(40),
-	id_taxonomia varchar(40),
-	PRIMARY KEY (identificacion_tax),
-	FOREIGN KEY (id_taxonomia) REFERENCES informacion_taxonomica
-);
-
-CREATE TABLE especies_amenzadas (
-	id_nomenclatura varchar(40),
-	estado_critico varchar(40),
-	codigo_int int,
-	PRIMARY KEY (id_nomenclatura),
-	FOREIGN KEY (codigo_int) REFERENCES intraespecificidad_epiteto
 );
