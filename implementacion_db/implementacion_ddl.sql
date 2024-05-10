e2d23316f75fc026abbe1c18ac7be9043bb92f89
Mariana Romero Reyes <165955733+M4lureyes@users.noreply.github.com>
2024-05-10 10:31:07 -0500

    DDL oficial
    Todas las tablas creadas con base a las correcciones realizadas en el MER y MR

---
+++ implementacion_db/implementacion_ddl.sql
@@ -0,0 +1,38 @@
+CREATE TABLE clasificacion_superior (
+	codigo integer,
+	reino varchar(40),
+	filio varchar(40),
+	clase varchar(40),
+	orden varchar(40),
+	familia varchar(40),
+	PRIMARY KEY (codigo)
+);
+
+CREATE TABLE informacion_taxonomica (
+	id_taxonomia varchar(40),
+	estado_taxonomico varchar(40),
+	observacion_taxonomica varchar(80),
+	rango_taxonomico varchar(40),
+	codigo_clasificacion integer,
+	PRIMARY KEY (id_taxonomia), 
+	FOREIGN KEY (codigo_clasificacion) REFERENCES clasificacion_superior
+);
+
+CREATE TABLE intraespecificidad_epiteto (
+	codigo_int integer,
+	genero varchar(40),
+	epiteto_especifico varchar(40),
+	epiteto_intraespecifico varchar(40),
+	nombre_vernaculo varchar(60),
+	codigo integer,
+	PRIMARY KEY (codigo_int),
+	FOREIGN KEY (codigo) REFERENCES clasificacion_superior
+);
+
+CREATE TABLE especies_amenazadas (
+	id_nomenclatura varchar(40),
+	estado_de_amenaza varchar(40),
+	codigo_int integer,
+	PRIMARY KEY (id_nomenclatura),
+	FOREIGN KEY (codigo_int) REFERENCES intraespecificidad_epiteto
+);
