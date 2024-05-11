1e331af3b4af56d160bccbbed8dac9ebfa67d263
Natalia48232 <81546880+Natalia48232@users.noreply.github.com>
2024-05-10 20:55:11 -0500

    Update implementacion_ddl.sql

--- implementacion_db/implementacion_ddl.sql
+++ implementacion_db/implementacion_ddl.sql
@@ -13,7 +13,7 @@ CREATE TABLE clasificacion_superior (
 CREATE TABLE informacion_taxonomica (
 	id_taxonomia varchar(40),
 	estado_taxonomico varchar(40),
-	observacion_taxonomica varchar(200),
+	observacion_taxonomica varchar(600),
 	rango_taxonomico varchar(40),
 	codigo_clasificacion varchar(40),
 	PRIMARY KEY (id_taxonomia), 
@@ -25,7 +25,7 @@ CREATE TABLE intraespecificidad_epiteto (
 	genero varchar(40),
 	epiteto_especifico varchar(40),
 	epiteto_intraespecifico varchar(40),
-	nombre_vernaculo varchar(150),
+	nombre_vernaculo varchar(500),
 	codigo integer,
 	PRIMARY KEY (codigo_int),
 	FOREIGN KEY (codigo) REFERENCES clasificacion_superior
